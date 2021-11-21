13a41fd0048e6777b9b42ffc499c7cc476f1f34b
Mayra Estrella <93684336+mayra84@users.noreply.github>
2021-11-21 11:01:19 -0600

    trying to make an rsvp thing let us see if it works

---
+++ _posts/thanksgiving.py
@@ -0,0 +1,6 @@
+guest_name = input('Please enter your name: ')
+
+guest_party = input('Please enter the names of your party members: ')
+
+guest_status = input('Will you be attending this year\'s Estrella-Bernal Thanksgiving Feast? ')
+
